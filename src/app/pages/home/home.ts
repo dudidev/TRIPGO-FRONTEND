c6255f6d36fd9ec0b95528625f4250f32c0cab44
Duvier SENA <duvierdiegodavid.0715@gmail.com>
2025-09-25 09:07:16 -0500

    Add components and routes

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+import { Header } from '../../shared/header/header';
+import { Nav } from '../../shared/nav/nav';
+import { Footer } from '../../shared/footer/footer';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Header, Nav, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-
+  protected readonly bienvenida = signal('Bienvenid@ a TripGO');
 }
