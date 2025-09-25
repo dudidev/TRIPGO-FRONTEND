87ec28edc9bbd4a2ae652202edc3729467a1eecd
Duvier SENA <duvierdiegodavid.0715@gmail.com>
2025-09-25 09:06:49 -0500

    Add routes to pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'Login', component: Login},
+    {path: 'Register', component: Register},
+    {path: '**', redirectTo:''}
+];
