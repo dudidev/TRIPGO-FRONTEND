07c5020dab3df3f85938dc0c9f8cfd12c72befa3
Duvansg06 <140823687+Duvansg06@users.noreply.github.com>
2025-10-01 11:11:06 -0500

    TODO MELO POR AHORA

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PrincipalComponent } from './pages/principal/principal.component';   
+import { NocturnaComponent } from './pages/nocturna/nocturna.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [  
+    { path : '', component: PrincipalComponent},
+    {path : 'principal', component: PrincipalComponent},
+    {path : 'nocturna', component: NocturnaComponent}
+];
