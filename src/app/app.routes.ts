6f8fb93acc47178a8a80d8e749a598bee92317ba
Karol Franco <francokarol696@gmail.com>
2025-10-01 10:45:07 -0500

    ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'Home', component: Home}
+];
