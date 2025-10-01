2bb0cf2f7cbd8a3d530dae9bca7ceaa11dc1119a
Karol Franco <francokarol696@gmail.com>
2025-10-01 10:44:31 -0500

    cambios karol

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Nav } from '../../shared/nav/nav';
+import { Footer } from '../../shared/footer/footer';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Nav,Footer],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
