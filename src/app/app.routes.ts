da9252fdba449b2658677a52696bf334573dbe9b
Duvier David <duvierdiegodavid.0715@gmail.com>
2025-10-01 22:28:59 -0500

    Refactor and delete innecesary routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,6 @@ import { NocturnaComponent } from './pages/nocturna/nocturna.component';
 
 
 import { Home } from './pages/home/home';
-import { About } from './pages/about/about';
 import { Contact } from './pages/contact/contact';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
@@ -14,7 +13,6 @@ export const routes: Routes = [
     {path: '', component: Home},
     {path: 'login', component: Login},
     {path: 'register', component: Register},
-    {path: 'about', component: About},
     {path: 'contact', component: Contact},
     {path : 'principal', component: PrincipalComponent},
     {path : 'nocturna', component: NocturnaComponent},
