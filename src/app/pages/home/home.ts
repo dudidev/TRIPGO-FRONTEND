eb1f53b72ebbd6e638619939e93113482a79bcf2
shaiCausil <causil845@gmail.com>
2025-10-02 14:34:28 -0500

    terminal clear

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,10 @@
 import { Component, signal} from '@angular/core';
-import { Nav } from '../../shared/nav/nav';
+
 import { Footer } from '../../shared/footer/footer';
 
 @Component({
   selector: 'app-home',
-  imports: [Nav,Footer],
+  imports: [Footer],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
