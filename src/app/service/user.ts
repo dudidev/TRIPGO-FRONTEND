beeb42e6048d268e69f52c50b62e93d5caec8059
Duvier David <duvierdiegodavid.0715@gmail.com>
2025-10-02 13:18:37 -0500

    Refactor Include currentUser to change nav style if be logged

--- src/app/service/user.ts
+++ src/app/service/user.ts
@@ -4,15 +4,33 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class User {
-    register(user: { name: string, email: string, password: string }) {
+  register(user: { name: string, email: string, password: string }) {
     let users = JSON.parse(localStorage.getItem('users') || '[]');
     users.push(user);
     localStorage.setItem('users', JSON.stringify(users));
   }
 
-  
+
   login(email: string, password: string): boolean {
     let users = JSON.parse(localStorage.getItem('users') || '[]');
-    return users.some((u: any) => u.email === email && u.password === password);
- }
+    const foundUser = users.find((u: any) => u.email === email && u.password === password);
+
+    if (foundUser) {
+      localStorage.setItem('currentUser', JSON.stringify(foundUser)); //Guarda el usuario logueado
+      return true;
+      }
+    return false;
   }
+
+  logout(): void {
+    localStorage.removeItem('currentUser');
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('currentUser');
+  }
+
+  getCurrentUser(): any {
+    return JSON.parse(localStorage.getItem('currentUser') || 'null');
+  }
+}
