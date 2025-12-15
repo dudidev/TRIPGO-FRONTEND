9055b55d726f46b9ed90d6756c1458505eff7493
Karol Franco <francokarol696@gmail.com>
2025-12-15 06:39:11 -0500

    Creando editar perfil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import {  RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { PrincipalComponent } from './pages/principal/principal.component';   
 import { Home } from './pages/home/home';
 import { Contact } from './pages/contact/contact';
@@ -7,8 +8,7 @@ import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
 import { Categorias } from './pages/categorias/categorias';
 import { Cabalgata } from './pages/cabalgata/cabalgata';
-
-
+import { EditarCuentaComponent } from './pages/editar-cuenta/editar-cuenta';
 
 
 export const routes: Routes = [
@@ -18,11 +18,13 @@ export const routes: Routes = [
     {path: 'contact', component: Contact},
     {path : 'principal', component: PrincipalComponent},
     {path: 'categorias/:slug', component: Categorias },
-{ path: 'cabalgata/:slug', component: Cabalgata }
+    { path: 'cabalgata/:slug', component: Cabalgata },
+    { path: 'editar-cuenta', component: EditarCuentaComponent }
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), ReactiveFormsModule],
   exports: [RouterModule]
 })
 export class AppRoutes {}
+export class AppModule {}
---
+++ src/app/pages/editar-cuenta/editar-cuenta.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Nav } from '../../shared/nav/nav';
+import { Footer } from '../../shared/footer/footer';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
+import { OnInit } from '@angular/core';
+@Component({
+  selector: 'app-editar-cuenta',
+  imports: [Nav,CommonModule,Footer,ReactiveFormsModule],
+  templateUrl: './editar-cuenta.html',
+  styleUrl: './editar-cuenta.css'
+})
+
+export class EditarCuentaComponent implements OnInit {
+
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+usuarioMock = {
+  nombre_usuario: 'Karol Franco',
+  correo_usuario: 'karol@gmail.com'
+};
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      nombre_usuario: ['Karol franco', Validators.required],
+      correo_usuario: ['karol@gmail.com', [Validators.required, Validators.email]]
+    });
+
+    // Datos simulados (luego vendrán del backend)
+    this.form.patchValue({
+      nombre_usuario: 'Usuario de prueba',
+      correo_usuario: 'correo@prueba.com'
+    });
+  }
+
+  guardar(): void {
+    console.log('Datos listos para enviar al backend:', this.form.value);
+  }
+}
