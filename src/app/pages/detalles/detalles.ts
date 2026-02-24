535fca2cff74ccc2cacba66beba0c07d94ca2692
Duvier David <duvierdiegodavid.0715@gmail.com>
2026-02-24 16:36:00 -0500

    Fix formatting and indentation in detalles.ts

--- src/app/pages/detalles/detalles.ts
+++ src/app/pages/detalles/detalles.ts
@@ -23,7 +23,7 @@ type LugarDetalle = {
   datosGenerales?: string[];
 
   // opcionales para navegación / breadcrumb
-    lat: number;
+  lat: number;
   lng: number;
   townSlug?: string;
   categoryKey?: string;
@@ -81,7 +81,7 @@ const DETALLES_DATA: Record<string, LugarDetalle> = {
     imagenes: [
       'https://res.cloudinary.com/dshqbl8d1/image/upload/v1765733201/portada_-_categoria_5_t7pmpz.jpg',
     ],
-   opiniones: [
+    opiniones: [
       {
         usuario: 'Pedro Fernandez',
         comentario: 'Excelente lugar, muy buena atención.',
@@ -113,7 +113,7 @@ const DETALLES_DATA: Record<string, LugarDetalle> = {
     imagenes: [
       'https://res.cloudinary.com/dshqbl8d1/image/upload/v1765733201/portada_-_categoria_5_t7pmpz.jpg',
     ],
-     opiniones: [
+    opiniones: [
       {
         usuario: 'Pedro Fernandez',
         comentario: 'Excelente lugar, muy buena atención.',
@@ -125,27 +125,27 @@ const DETALLES_DATA: Record<string, LugarDetalle> = {
           'Me encantó la experiencia, los paisajes son hermosos, pero podrían mejorar un poco la organización. En general, ¡muy recomendable!',
         rating: 4,
       },
-    ], 
+    ],
     townSlug: 'salento',
     categoryKey: 'cabalgatas',
-    lat:   4.654863733626751,
+    lat: 4.654863733626751,
     lng: -75.5757018934317,
   },
   'COCORATOURS1': {
     slug: 'COCORATOURS1',
     nombre: 'CocoraTours - Excursión grupal valle del cocora',
     direccion: 'Direccion: barrio las Colinas, calle 2 # 6-09, Salento Quindio. ',
-    descripcion:'Este tour económico incluye un recorrido en jeep tradicional hasta el Valle de Cocora, donde iniciaremos una caminata por diversas atracciones artificiales perfectas para tomarte fotos memorables. Entre ellas encontrarás el famoso jeep con la bandera y costales de café, el letrero Cocora, las mallas, la casa tradicional y esculturas de fauna local como el puma, el oso, el venado y el emblemático nido del cóndor.'+'\n'+'\n'+
-     'También podrás conocer la famosa mano azul con fondo de palmas de cera. Si prefieres un recorrido más natural y menos fotográfico, puedes omitir estas paradas y adaptar la experiencia a tu gusto.El recorrido continúa con un tour caminando de aproximadamente 2 a 3 horas por los hermosos valles de palmas, pasando por miradores naturales que ofrecen vistas panorámicas de 360 grados sobre todo el Valle de Cocora.'+'\n'+'\n'+ 'Durante la caminata,' +
-     'disfrutarás de paisajes únicos, cuencas de ríos y las majestuosas palmas de cera, el árbol nacional más alto de Colombia. Este tour económico es perfecto para quienes desean explorar la naturaleza, capturar momentos increíbles y vivir una experiencia auténtica sin afectar demasiado su presupuesto.',
+    descripcion: 'Este tour económico incluye un recorrido en jeep tradicional hasta el Valle de Cocora, donde iniciaremos una caminata por diversas atracciones artificiales perfectas para tomarte fotos memorables. Entre ellas encontrarás el famoso jeep con la bandera y costales de café, el letrero Cocora, las mallas, la casa tradicional y esculturas de fauna local como el puma, el oso, el venado y el emblemático nido del cóndor.' + '\n' + '\n' +
+      'También podrás conocer la famosa mano azul con fondo de palmas de cera. Si prefieres un recorrido más natural y menos fotográfico, puedes omitir estas paradas y adaptar la experiencia a tu gusto.El recorrido continúa con un tour caminando de aproximadamente 2 a 3 horas por los hermosos valles de palmas, pasando por miradores naturales que ofrecen vistas panorámicas de 360 grados sobre todo el Valle de Cocora.' + '\n' + '\n' + 'Durante la caminata,' +
+      'disfrutarás de paisajes únicos, cuencas de ríos y las majestuosas palmas de cera, el árbol nacional más alto de Colombia. Este tour económico es perfecto para quienes desean explorar la naturaleza, capturar momentos increíbles y vivir una experiencia auténtica sin afectar demasiado su presupuesto.',
     datosGenerales: [
       'Duración: 4-5 horas. ',
       'Dificultad: Media. ',
       'Transporte ida y regreso en Jeep Willys. ',
       'Entrada al bosque de palmas( Mano, mallas, nido, miradores).',
       'Guia turistico local. '
-    ], 
-    
+    ],
+
     promociones: '',
     horarios: [
       'Lunes a Viernes: 9:00 AM - 5:00 PM',
@@ -156,7 +156,7 @@ const DETALLES_DATA: Record<string, LugarDetalle> = {
       'https://res.cloudinary.com/dtyvd3fim/image/upload/v1770910663/01-02LUGAR_gbdupb.png',
       'https://res.cloudinary.com/dtyvd3fim/image/upload/v1770910663/01-03LUGAR_hzgc9d.png',
     ],
-     opiniones: [
+    opiniones: [
       {
         usuario: 'Pedro Fernandez',
         comentario: 'Excelente lugar, muy buena atención.',
@@ -171,7 +171,7 @@ const DETALLES_DATA: Record<string, LugarDetalle> = {
     ],
     townSlug: 'salento',
     categoryKey: 'valle-cocora',
-    lat:4.6389628880699325,
+    lat: 4.6389628880699325,
     lng: -75.56772317805938,
   },
 
@@ -190,7 +190,7 @@ export class Detalles {
   // lugar actual (o null si no existe)
   lugar: LugarDetalle | null = null;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
