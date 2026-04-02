66e2617b4469f1aac467da42c3fc8b367007ad3a
Duvier David <duvierdiegodavid.0715@gmail.com>
2026-04-02 16:35:21 -0500

    Add TranslateModule and LanguageSwitcher to Nav component for internationalization support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,37 @@
-import { ApplicationConfig, isDevMode } from '@angular/core';
+import { ApplicationConfig, isDevMode, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './interceptors/auth-interceptor';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { languageInterceptor } from './interceptors/language-interceptor';
 import { provideServiceWorker } from '@angular/service-worker';
+
+import { routes } from './app.routes';
+import { authInterceptor } from './interceptors/auth-interceptor';
+
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor, languageInterceptor])), provideServiceWorker('ngsw-worker.js', {
-            enabled: !isDevMode(),
-            registrationStrategy: 'registerWhenStable:30000'
-          }), provideAnimations()
+    provideHttpClient(withInterceptors([authInterceptor, languageInterceptor])),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })/*,
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        defaultLanguage: 'es',
+        loader: {
+          provide: TranslateLoader,
+          useValue: {
+            getTranslation: (lang: string) => {
+              return fetch(`./assets/i18n/${lang}.json`).then(res => res.json());
+            }
+          }
+        }
+      })
+    )
+      */
   ]
 };
