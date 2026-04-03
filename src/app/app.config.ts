467b9297de0f2d40825755b04c7b6099e6c6d19d
Duvier David <duvierdiegodavid.0715@gmail.com>
2026-04-02 19:11:45 -0500

    Refactor app configuration to improve structure and enhance TranslateModule integration for internationalization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,37 +1,55 @@
-import { ApplicationConfig, isDevMode, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
+import { provideRouter} from '@angular/router';
 
+// Import common services
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// Import custom interceptors
+import { authInterceptor} from './interceptors/auth-interceptor';
 import { languageInterceptor } from './interceptors/language-interceptor';
+
+// Import service worker configuration
 import { provideServiceWorker } from '@angular/service-worker';
 
+// Define routes for the application
 import { routes } from './app.routes';
-import { authInterceptor } from './interceptors/auth-interceptor';
 
+// Import animations module
 import { provideAnimations } from '@angular/platform-browser/animations';
 
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+// Export the application configuration object
 export const appConfig: ApplicationConfig = {
   providers: [
+    // Provide the router with the defined routes
     provideRouter(routes),
+    
+    // Apply Angular animations
+    provideAnimations(),
+    
+    // Configure HTTP client with interceptors
     provideHttpClient(withInterceptors([authInterceptor, languageInterceptor])),
+    
+    // Set up service worker for progressive web app support
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000'
-    })/*,
+    }),
     importProvidersFrom(
-      TranslateModule.forRoot({
-        defaultLanguage: 'es',
-        loader: {
-          provide: TranslateLoader,
-          useValue: {
-            getTranslation: (lang: string) => {
-              return fetch(`./assets/i18n/${lang}.json`).then(res => res.json());
-            }
-          }
-        }
-      })
-    )
-      */
+    TranslateModule.forRoot({
+      defaultLanguage: 'es',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  )
   ]
 };
