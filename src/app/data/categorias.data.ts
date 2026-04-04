6fe966572f6f14d204296f1a4880f7a8d298e5fc
Karol Franco <francokarol696@gmail.com>
2026-04-04 18:46:46 -0500

    Imagenes listas

--- src/app/data/categorias.data.ts
+++ src/app/data/categorias.data.ts
@@ -118,7 +118,11 @@ export const CATEGORIAS_DATA: Record<string, CategoriaData> = {
   circasia: {
     nombre: 'Circasia',
     sliderImgs: [
-      'https://res.cloudinary.com/dshqbl8d1/image/upload/v1773406545/img_de_fondo_para_categorias_pyucuh.jpg',
+          'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775159898/Circasia5_amvl8a.webp', 
+          'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775159896/Circasia1_rp7bma.jpg', 
+          'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775260618/Circasia8_jrftwp.jpg', 
+          'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775260618/Circasia6_h7zleg.jpg',
+          'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775260618/Circasia7_lluxq8.jpg'
     ],
     categorias: [
       { key: 'miradores', label: 'Miradores', img: 'https://via.placeholder.com/600x400?text=MIRADORES' },
@@ -134,7 +138,16 @@ export const CATEGORIAS_DATA: Record<string, CategoriaData> = {
   armenia: {
     nombre: 'Armenia',
     sliderImgs: [
-      'https://res.cloudinary.com/dshqbl8d1/image/upload/v1773406545/img_de_fondo_para_categorias_pyucuh.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775261612/Armenia3_cq6fwk.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775261612/Armenia6_xeq6px.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775260618/Circasia7_lluxq8.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775261612/Armenia2_jwvnhk.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775261612/Armenia1_lilggn.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775261612/Armenia4_bsjt0h.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775262112/Armenia8_lmk6vq.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775262125/Armenia5_xfemej.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775262112/Armenia7_o4ajef.jpg'
+
     ],
     categorias: [
       { key: 'miradores', label: 'Miradores', img: 'https://via.placeholder.com/600x400?text=MIRADORES' },
@@ -150,7 +163,12 @@ export const CATEGORIAS_DATA: Record<string, CategoriaData> = {
   buenavista: {
     nombre: 'Buenavista',
     sliderImgs: [
-      'https://res.cloudinary.com/dshqbl8d1/image/upload/v1773406545/img_de_fondo_para_categorias_pyucuh.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775264989/BV6_kk4czp.webp',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775264988/BV5_jcwbm7.webp',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775264987/BV3_xzhmjl.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775264987/BV2_dplaga.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775264959/BV1_mj9mw5.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775264987/BV4_gbp0cw.jpg'
     ],
     categorias: [
       { key: 'miradores', label: 'Miradores', img: 'https://via.placeholder.com/600x400?text=MIRADORES' },
@@ -166,7 +184,12 @@ export const CATEGORIAS_DATA: Record<string, CategoriaData> = {
   pijao: {
     nombre: 'Pijao',
     sliderImgs: [
-      'https://res.cloudinary.com/dshqbl8d1/image/upload/v1773406545/img_de_fondo_para_categorias_pyucuh.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775332519/Pijao1_q9jvz0.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775332518/Pijao2_phonkg.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775332519/Pijao3_bggsum.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775332519/Pijao4_cqvby0.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775332522/Pijao5_ykfebz.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775332523/Pijao6_ihjxuv.jpg'
     ],
     categorias: [
       { key: 'miradores', label: 'Miradores', img: 'https://via.placeholder.com/600x400?text=MIRADORES' },
@@ -182,7 +205,12 @@ export const CATEGORIAS_DATA: Record<string, CategoriaData> = {
   cordoba: {
     nombre: 'Cordoba',
     sliderImgs: [
-      'https://res.cloudinary.com/dshqbl8d1/image/upload/v1773406545/img_de_fondo_para_categorias_pyucuh.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775336786/Cordoba1_m2btyo.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775336572/Cordoba2_onlncv.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775336572/Cordoba3_u6w03u.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775336574/Cordoba5_urgxif.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775336574/Cordoba6_jidtmp.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775336697/Cordoba4_sfzola.jpg'
     ],
     categorias: [
       { key: 'miradores', label: 'Miradores', img: 'https://via.placeholder.com/600x400?text=MIRADORES' },
@@ -198,7 +226,11 @@ export const CATEGORIAS_DATA: Record<string, CategoriaData> = {
   tebaida: {
     nombre: 'Latebaida',
     sliderImgs: [
-      'https://res.cloudinary.com/dshqbl8d1/image/upload/v1773406545/img_de_fondo_para_categorias_pyucuh.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775337553/LT1_klkaww.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775337553/LT2_bkwbfc.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775337554/LT3_j6skh4.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775337554/LT4_hxbuzz.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775337556/LT5_s5ujvf.jpg'
     ],
     categorias: [
       { key: 'miradores', label: 'Miradores', img: 'https://via.placeholder.com/600x400?text=MIRADORES' },
@@ -214,7 +246,11 @@ export const CATEGORIAS_DATA: Record<string, CategoriaData> = {
   genova: {
     nombre: 'Genova',
     sliderImgs: [
-      'https://res.cloudinary.com/dshqbl8d1/image/upload/v1773406545/img_de_fondo_para_categorias_pyucuh.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775345592/Genova5_scr1ck.avif',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775345592/Genova3_l0ssxl.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775345592/Genova2_tceb0y.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775345592/Genova4_ju8kdr.jpg',
+      'https://res.cloudinary.com/dtyvd3fim/image/upload/v1775346209/Genova6_awyoxw.jpg'
     ],
     categorias: [
       { key: 'miradores', label: 'Miradores', img: 'https://via.placeholder.com/600x400?text=MIRADORES' },
