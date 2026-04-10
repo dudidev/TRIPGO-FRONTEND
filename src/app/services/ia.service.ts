2975425f67828da72ac4d69cdddc4fbcf95cec63
Duvier David <duvierdiegodavid.0715@gmail.com>
2026-04-09 21:46:07 -0500

    refactor(chatbot): mover consumo de IA al service usando environment

--- src/app/services/ia.service.ts
+++ src/app/services/ia.service.ts
@@ -1,18 +1,18 @@
-import { Injectable } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
-import { Observable } from 'rxjs'
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AiService {
 
-  private apiUrl = 'https://tripgo-backend-arehbhbubshxdpg7.chilecentral-01.azurewebsites.net/ia/chatbot'
+  private readonly apiUrl = `${environment.apiBaseUrl}/ia/chatbot`;
 
   constructor(private http: HttpClient) {}
 
   preguntarIA(mensaje: string): Observable<any> {
-    return this.http.post(this.apiUrl, { mensaje })
+    return this.http.post<any>(this.apiUrl, { mensaje });
   }
-
 }
