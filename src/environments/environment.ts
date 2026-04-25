5a6d0eadccf51b4256f630cf42f899cbce284296
Duvier David <duvierdiegodavid.0715@gmail.com>
2026-04-24 23:40:31 -0500

    fix(environment): update apiBaseUrl to staging for development

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://tripgo-backend-production.up.railway.app' // Cambia a la URL de tu backend en producción
+  apiBaseUrl: 'https://tripgo-backend-staging.up.railway.app' // Cambia a la URL de tu backend en staging
   //apiBaseUrl: 'http://localhost:8080'
 };  
